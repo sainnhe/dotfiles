409b8be7c9aee08adf08d0ddadedba02f188aa9e
Sainnhepark <sainnhe@gmail.com>
2019-08-19 12:55:11 +0800

    setup: fix dotfiles-setup.sh

--- setup/dotfiles-setup.sh
+++ setup/dotfiles-setup.sh
@@ -20,25 +20,25 @@ setup_copy() {
 }
 #}}}
 
-setup_symlink ../.Xresources
-setup_copy ../.redshiftgrc
-setup_symlink ../.haxornewsconfig
-setup_symlink ../.gitsomeconfig
-setup_copy ../.gitconfig
-setup_copy ../.ctags
-setup_symlink ../.conkyrc
-setup_symlink ../.bashrc
-setup_symlink ../.w3m
-setup_symlink ../.local/share/applications
-setup_copy ../.aria2
-setup_symlink ../.config/alacritty/alacritty.yml
-setup_copy ../.config/autostart
-setup_symlink ../.config/browsh/config.toml
-setup_symlink ../.config/fontconfig/fonts.conf
-setup_symlink ../.config/polybar
-setup_symlink ../.config/rofi
-setup_symlink ../.config/rtv
-setup_symlink ../.config/compton.conf
-setup_copy ../.config/pikaur.conf
+setup_symlink .Xresources
+setup_copy .redshiftgrc
+setup_symlink .haxornewsconfig
+setup_symlink .gitsomeconfig
+setup_copy .gitconfig
+setup_copy .ctags
+setup_symlink .conkyrc
+setup_symlink .bashrc
+setup_symlink .w3m
+setup_symlink .local/share/applications
+setup_copy .aria2
+setup_symlink .config/alacritty/alacritty.yml
+setup_copy .config/autostart
+setup_symlink .config/browsh/config.toml
+setup_symlink .config/fontconfig/fonts.conf
+setup_symlink .config/polybar
+setup_symlink .config/rofi
+setup_symlink .config/rtv
+setup_symlink .config/compton.conf
+setup_copy .config/pikaur.conf
 
 echo "setup root directory manually"
