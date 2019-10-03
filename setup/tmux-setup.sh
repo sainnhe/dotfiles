02d299920ff299660aa3e5e8ed62e0874d0bc891
Sainnhepark <sainnhe@gmail.com>
2019-10-03 16:18:07 +0800

    setup(tmux): remove tmux-bind.sh

--- setup/tmux-setup.sh
+++ setup/tmux-setup.sh
@@ -5,7 +5,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 mkdir -p ~/.tmux
 ln -s /home/sainnhe/repo/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s /home/sainnhe/repo/dotfiles/.tmux-bind.sh ~/.tmux-bind.sh
 ln -s /home/sainnhe/repo/dotfiles/.tmux/tmuxline ~/.tmux/tmuxline
 
 echo "'prefix I' to install plugins"
