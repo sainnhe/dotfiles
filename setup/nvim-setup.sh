e5ba26e0f0132bda3fa7ec4251d8a903364a8b89
Sainnhepark <sainnhe@gmail.com>
2019-11-16 18:22:51 +0800

    setup(nvim): update

--- setup/nvim-setup.sh
+++ setup/nvim-setup.sh
@@ -67,8 +67,6 @@ pacman_setup_func() {
     pacman -S --noconfirm uncrustify &>/dev/null
     echo "installing yapf..."
     pacman -S --noconfirm yapf &>/dev/null
-    echo "installing python-language-server..."
-    pacman -S --noconfirm python-language-server &>/dev/null
     echo "installing bash-language-server..."
     pacman -S --noconfirm bash-language-server &>/dev/null
     echo "installing hasktags..."
@@ -81,75 +79,37 @@ pacman_setup_func() {
 pikaur_setup_func() {
     echo -n "make sure you have configured makepkg proxy correctly. [enter to continue]  "
     read -r
-    echo -n "install microsoft-python-language-server from AUR. [enter to continue]  "
-    read -r
-    pikaur -S microsoft-python-language-server
-    echo -n "install javascript-typescript-langserver from AUR. [enter to continue]  "
-    read -r
-    pikaur -S javascript-typescript-langserver
-    echo -n "install yaml-language-server-bin from AUR. [enter to continue]  "
-    read -r
-    pikaur -S yaml-language-server-bin
-    echo -n "install global from AUR. [enter to continue]  "
-    read -r
-    pikaur -S global
-    echo -n "install toilet from AUR. [enter to continue]  "
-    read -r
-    pikaur -S toilet
-    echo -n "install toilet-fonts from AUR. [enter to continue]  "
-    read -r
-    pikaur -S toilet-fonts
-    echo -n "install stylelint from AUR. [enter to continue]  "
-    read -r
-    pikaur -S stylelint
-    echo -n "install stylelint-config-standard from AUR. [enter to continue]  "
-    read -r
-    pikaur -S stylelint-config-standard
-    echo -n "install nodejs-jsonlint from AUR. [enter to continue]  "
-    read -r
-    pikaur -S nodejs-jsonlint
-    echo -n "install js-beautify from AUR. [enter to continue]  "
-    read -r
-    pikaur -S js-beautify
-    echo -n "install universal-ctags-git from AUR. [enter to continue]  "
-    read -r
-    pikaur -S universal-ctags-git
-    echo -n "install gotags-git from AUR. [enter to continue]  "
-    read -r
-    pikaur -S gotags-git
-    echo -n "install jsctags-tern-git from AUR. [enter to continue]  "
-    read -r
-    pikaur -S jsctags-tern-git
-    echo -n "install markdown2ctags from AUR. [enter to continue]  "
-    read -r
-    pikaur -S markdown2ctags
-    echo -n "install rst2ctags from AUR. [enter to continue]  "
-    read -r
-    pikaur -S rst2ctags
-    echo -n "install cling-git from AUR. [enter to continue]  "
-    read -r
-    pikaur -S cling-git
-}
-# }}}
-# {{{npm_yarn_setup()
-npm_yarn_setup() {
-    cd  ~
-    echo -n "Use proxychains? [Y/n]  "
-    read -r var
-    if [[ "$var"x == ""x ]]; then
-        var=Y
-    fi
-    if [[ "$var" == "Y" ]]; then
-        proxychains -q npm install --user vscode-html-languageserver-bin
-        proxychains -q npm install --user vscode-css-languageserver-bin
-        proxychains -q npm install --user vscode-json-languageserver-bin
-        proxychains -q yarn add --dev flow-bin
-    else
-        npm install --user vscode-html-languageserver-bin
-        npm install --user vscode-css-languageserver-bin
-        npm install --user vscode-json-languageserver-bin
-        yarn add --dev flow-bin
-    fi
+    pikaur -S global stylelint stylelint-config-standard nodejs-jsonlint js-beautify universal-ctags-git gotags-git jsctags-tern-git markdown2ctags rst2ctags
+    # echo -n "install global from AUR. [enter to continue]  "
+    # read -r
+    # pikaur -S global
+    # echo -n "install stylelint from AUR. [enter to continue]  "
+    # read -r
+    # pikaur -S stylelint
+    # echo -n "install stylelint-config-standard from AUR. [enter to continue]  "
+    # read -r
+    # pikaur -S stylelint-config-standard
+    # echo -n "install nodejs-jsonlint from AUR. [enter to continue]  "
+    # read -r
+    # pikaur -S nodejs-jsonlint
+    # echo -n "install js-beautify from AUR. [enter to continue]  "
+    # read -r
+    # pikaur -S js-beautify
+    # echo -n "install universal-ctags-git from AUR. [enter to continue]  "
+    # read -r
+    # pikaur -S universal-ctags-git
+    # echo -n "install gotags-git from AUR. [enter to continue]  "
+    # read -r
+    # pikaur -S gotags-git
+    # echo -n "install jsctags-tern-git from AUR. [enter to continue]  "
+    # read -r
+    # pikaur -S jsctags-tern-git
+    # echo -n "install markdown2ctags from AUR. [enter to continue]  "
+    # read -r
+    # pikaur -S markdown2ctags
+    # echo -n "install rst2ctags from AUR. [enter to continue]  "
+    # read -r
+    # pikaur -S rst2ctags
 }
 # }}}
 rust_setup() { #{{{
@@ -163,8 +123,6 @@ if [ "$1" = "pacman" ]; then
     pacman_setup_func
 elif [ "$1" = "pikaur" ]; then
     pikaur_setup_func
-elif [ "$1" = "node" ]; then
-    npm_yarn_setup
 elif [ "$1" = "rust" ]; then
     rust_setup
 elif [ "$1" = "dotfiles" ]; then
