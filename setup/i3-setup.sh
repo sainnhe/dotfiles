6e99291c3207ea7f1a28bda872e33dc57edd7267
Sainnhepark <sainnhe@gmail.com>
2019-11-16 22:45:52 +0800

    setup(i3): use lxappearance-gtk3

--- setup/i3-setup.sh
+++ setup/i3-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-pikaur -S i3-gaps xorg-xrdb polybar lxappearance feh perl-anyevent-i3 compton-tryone-git redshiftgui-bin nm-connection-editor blueman gbacklight rofi
+pikaur -S i3-gaps xorg-xrdb polybar lxappearance-gtk3 feh perl-anyevent-i3 compton-tryone-git redshiftgui-bin nm-connection-editor blueman gbacklight rofi
 
 mkdir -p ~/.config
 ln -s /home/sainnhe/repo/dotfiles/.config/i3 ~/.config/i3
