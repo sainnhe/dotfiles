f73cef9ba34f51856e3ba3ba7fffd4802c19a8a8
Sainnhepark <sainnhe@gmail.com>
2019-11-17 21:19:00 +0800

    setup(theme): add x-arc

--- setup/theme-setup.sh
+++ setup/theme-setup.sh
@@ -2,4 +2,4 @@
 
 pikaur -S capitaine-cursors numix-cursor-theme
 pikaur -S papirus-icon-theme mcmojave-circle-icon-theme-git
-pikaur -S arc-gtk-theme gtk-theme-arc-gruvbox-git snow-theme-git nordic-theme-git mojave-gtk-theme-git qogir-gtk-theme-git
+pikaur -S arc-gtk-theme gtk-theme-arc-gruvbox-git snow-theme-git nordic-theme-git mojave-gtk-theme-git qogir-gtk-theme-git x-arc-plus x-arc-white x-arc-darker x-arc-shadow
