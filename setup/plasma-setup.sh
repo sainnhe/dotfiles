8c711f8f1339e708c805befa38049ff129e0dfcc
Sainnhepark <sainnhe@gmail.com>
2020-03-16 10:59:21 +0800

    setup(plasma): install latte-dock

--- setup/plasma-setup.sh
+++ setup/plasma-setup.sh
@@ -4,6 +4,7 @@ sudo pacman -S plasma
 sudo pacman -S kde-applications
 sudo pacman -S kvantum-qt5
 sudo pacman -S plasma5-applets-active-window-control
+sudo pacman -S latte-dock
 pikaur -S capitaine-cursors numix-cursor-theme
 mkdir -p ~/repo
 cd ~/repo || exit
