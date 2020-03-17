d4e5b2db6dc08526d1b55ccd3f2f564f2541f0b4
Sainnhepark <sainnhe@gmail.com>
2020-03-17 10:50:45 +0800

    setup(plasma): remove cursor themes

--- setup/plasma-setup.sh
+++ setup/plasma-setup.sh
@@ -5,7 +5,6 @@ sudo pacman -S kde-applications
 sudo pacman -S kvantum-qt5
 sudo pacman -S plasma5-applets-active-window-control
 sudo pacman -S latte-dock
-pikaur -S capitaine-cursors numix-cursor-theme
 mkdir -p ~/repo
 cd ~/repo || exit
 git clone --depth 1 https://github.com/vinceliuice/McMojave-circle.git
