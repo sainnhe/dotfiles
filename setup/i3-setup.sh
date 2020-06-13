5f38b5737816a36550421afe25b2f4988f8827f0
Sainnhepark <sainnhe@gmail.com>
2020-06-13 21:54:54 +0800

    picom: init

--- setup/i3-setup.sh
+++ setup/i3-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-pikaur -S i3-gaps xorg-xrdb polybar lxappearance-gtk3 feh perl-anyevent-i3 picom-git redshiftgui-bin nm-connection-editor blueman gbacklight rofi
+pikaur -S i3-gaps xorg-xrdb polybar lxappearance-gtk3 feh perl-anyevent-i3 picom-ibhagwan-git redshiftgui-bin nm-connection-editor blueman gbacklight rofi
