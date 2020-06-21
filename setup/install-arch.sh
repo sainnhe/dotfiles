cffaa6c3317c3dac299df0db7faf69cc58599d75
Sainnhepark <sainnhe@gmail.com>
2020-06-21 21:12:11 +0800

    nvidia: remove nvidia package, use dkms version instead

--- setup/install-arch.sh
+++ setup/install-arch.sh
@@ -197,7 +197,7 @@ elif [ "$1" = "user" ]; then
     sudo systemctl enable surface-dtx-daemon.service
     # Nvidia
     # pikaur -S nvidia-beta-dkms nvidia-settings-beta nvidia-utils-beta opencl-nvidia-beta lib32-nvidia-utils-beta lib32-opencl-nvidia-beta bumblebee
-    sudo pacman -S nvidia nvidia-dkms nvidia-settings nvidia-utils opencl-nvidia lib32-nvidia-utils lib32-opencl-nvidia bumblebee
+    sudo pacman -S nvidia-dkms nvidia-settings nvidia-utils opencl-nvidia lib32-nvidia-utils lib32-opencl-nvidia bumblebee
     sudo pacman -S virtualgl lib32-virtualgl lib32-primus lib32-primus_vk primus primus_vk
     sudo mkdir -p /etc/modprobe.d
     sudo mkdir -p /etc/X11/xorg.conf.d
