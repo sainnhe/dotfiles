fd633c05f939dcb0e6acd32bdcf160ed5e4d1624
Sainnhepark <sainnhe@gmail.com>
2020-07-24 10:17:03 +0800

    nvim: format

--- setup/nvim.sh
+++ setup/nvim.sh
@@ -4,91 +4,65 @@
 pacman_setup_func() {
     echo -n "make sure you have configured proxy and locale correctly. [enter to continue]  "
     read -r
-    echo "updating cache..."
-    pacman -Syy &>/dev/null
-    echo "setting up python provider..."
-    pacman -S --noconfirm python python2 &>/dev/null
-    pacman -S --noconfirm python-pynvim &>/dev/null
-    pacman -S --noconfirm python2-neovim &>/dev/null
-    pacman -S --noconfirm python-pip python2-pip &>/dev/null
-    echo "installing boost..."
-    pacman -S --noconfirm boost &>/dev/null
-    echo "installing words..."
-    pacman -S --noconfirm words &>/dev/null
-    echo "installing python-wcwidth..."
-    pacman -S --noconfirm python-wcwidth &>/dev/null
-    echo "installing clang..."
-    pacman -S --noconfirm clang &>/dev/null
-    echo "installing ccls..."
-    pacman -S --noconfirm ccls &>/dev/null
-    echo "installing tidy..."
-    pacman -S --noconfirm tidy &>/dev/null
-    echo "installing jedi..."
-    pacman -S --noconfirm python-jedi &>/dev/null
-    echo "installing pylint..."
-    pacman -S --noconfirm python-pylint python2-pylint &>/dev/null
-    echo "installing flake8..."
-    pacman -S --noconfirm flake8 python2-flake8 &>/dev/null
-    echo "installing mypy..."
-    pacman -S --noconfirm mypy &>/dev/null
-    echo "installing pycodestyle..."
-    pacman -S --noconfirm python-pycodestyle python2-pycodestyle &>/dev/null
-    echo "installing pydocstyle..."
-    pacman -S --noconfirm python-pydocstyle python2-pydocstyle &>/dev/null
-    echo "installing flawfinder..."
-    pacman -S --noconfirm flawfinder &>/dev/null
-    echo "installing cppcheck..."
-    pacman -S --noconfirm cppcheck &>/dev/null
-    echo "installing shellcheck..."
-    pacman -S --noconfirm shellcheck &>/dev/null
-    echo "installing vint..."
-    pacman -S --noconfirm vint &>/dev/null
-    echo "installing astyle..."
-    pacman -S --noconfirm astyle &>/dev/null
-    echo "installing prettier..."
-    pacman -S --noconfirm prettier &>/dev/null
-    echo "installing shfmt..."
-    pacman -S --noconfirm shfmt &>/dev/null
-    echo "installing uncrustify..."
-    pacman -S --noconfirm uncrustify &>/dev/null
-    echo "installing yapf..."
-    pacman -S --noconfirm yapf &>/dev/null
-    echo "installing zenity..."
-    pacman -S --noconfirm zenity &>/dev/null
+    sudo pacman -Sy
+    sudo pacman -S \
+        neovim \
+        python \
+        python2 \
+        python-pynvim \
+        python2-neovim \
+        python-pip \
+        python2-pip \
+        boost \
+        words \
+        python-wcwidth \
+        clang \
+        ccls \
+        tidy \
+        python-jedi \
+        python-pylint python2-pylint \
+        flake8 python2-flake8 \
+        mypy \
+        python-pycodestyle python2-pycodestyle \
+        python-pydocstyle python2-pydocstyle \
+        flawfinder \
+        cppcheck \
+        shellcheck \
+        vint \
+        astyle \
+        prettier \
+        shfmt \
+        uncrustify \
+        yapf \
+        zenity
 }
 # }}}
 # {{{pikaur_setup_func()
 pikaur_setup_func() {
-    echo -n "make sure you have configured makepkg proxy correctly. [enter to continue]  "
+    echo -n "make sure you have configured the proxy of makepkg and pikaur correctly. [enter to continue]  "
     read -r
-    pikaur -S ruby-neovim global stylelint stylelint-config-standard nodejs-jsonlint js-beautify universal-ctags-git
-    # echo -n "install global from AUR. [enter to continue]  "
-    # read -r
-    # pikaur -S global
-    # echo -n "install stylelint from AUR. [enter to continue]  "
-    # read -r
-    # pikaur -S stylelint
-    # echo -n "install stylelint-config-standard from AUR. [enter to continue]  "
-    # read -r
-    # pikaur -S stylelint-config-standard
-    # echo -n "install nodejs-jsonlint from AUR. [enter to continue]  "
-    # read -r
-    # pikaur -S nodejs-jsonlint
-    # echo -n "install js-beautify from AUR. [enter to continue]  "
-    # read -r
-    # pikaur -S js-beautify
-    # echo -n "install universal-ctags-git from AUR. [enter to continue]  "
-    # read -r
-    # pikaur -S universal-ctags-git
+    pikaur -S \
+        ruby-neovim \
+        global \
+        stylelint \
+        stylelint-config-standard \
+        nodejs-jsonlint \
+        js-beautify \
+        universal-ctags-git
 }
 # }}}
 rust_setup() { #{{{
     sudo pacman -S rustup
     proxychains -q rustup install nightly
     rustup default nightly
-    proxychains -q rustup component add rust-analyzer-preview rust-analysis rust-src rustfmt
+    proxychains -q rustup component add \
+        rust-analyzer-preview \
+        rust-analysis \
+        rust-src \
+        rustfmt
 }
 # }}}
+#{{{ $ bash setup/nvim.sh pacman/pikaur/rust/dotfiles/other/fonts
 if [ "$1" = "pacman" ]; then
     pacman_setup_func
 elif [ "$1" = "pikaur" ]; then
@@ -137,3 +111,4 @@ elif [ "$1" = "fonts" ]; then
     cp ~/repo/icursive-nerd-font-non-free/Cascadia\ Code\ iCursive\ Cg/* ~/.local/share/fonts/
     fc-cache
 fi
+#}}}
