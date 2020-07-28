f143a33c779a186dbadc0f9d5383bad9bac0bf06
Sainnhepark <sainnhe@gmail.com>
2020-07-28 09:47:54 +0800

    setup(vim): install coc from AUR

--- setup/vim.sh
+++ setup/vim.sh
@@ -1,3 +1,11 @@
 #!/bin/env bash
 
-pikaur -S gruvbox-material-git
+pikaur -S \
+    gruvbox-material-git \
+    vim-onedark-git \
+    vim-polyglot-git \
+    vim-leaderf-git \
+    vim-coc \
+    vim-coc-extras-meta-git \
+    vim-coc-git-git \
+    vim-coc-pairs-git
