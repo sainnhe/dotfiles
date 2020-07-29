091116a1b197813ae8892645a0c195f8a010c0c8
Sainnhepark <sainnhe@gmail.com>
2020-07-29 20:19:22 +0800

    vim: install coc from AUR

--- setup/vim.sh
+++ setup/vim.sh
@@ -3,9 +3,23 @@
 pikaur -S \
     gruvbox-material-git \
     vim-selenized-git \
+    vim-solarized8-git \
     vim-onedark-git \
     vim-polyglot-git \
     vim-leaderf-git \
     vim-coc \
     vim-coc-extras-meta-git \
-    vim-coc-git-git
+    vim-coc-dictionary-git \
+    vim-coc-explorer-git \
+    vim-coc-git-git \
+    vim-coc-pairs-git \
+    vim-coc-clangd-git \
+    vim-coc-pyright-git \
+    vim-coc-tsserver-git \
+    vim-coc-vimlsp-git \
+    vim-coc-yaml-git \
+    vim-coc-json-git \
+    vim-coc-html-git \
+    vim-coc-css-git \
+    vim-coc-sh-git \
+    vim-coc-rust-analyzer-git
