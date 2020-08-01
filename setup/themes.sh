7eab4fc2a186991ca5dbf455bcbba9cc307742f6
Sainnhepark <sainnhe@gmail.com>
2020-08-01 17:15:32 +0800

    themes: more plasma themes

--- setup/themes.sh
+++ setup/themes.sh
@@ -39,5 +39,10 @@ git clone --depth 1 https://github.com/vinceliuice/Canta-theme.git
 sh ~/repo/Canta-theme/install.sh -i
 git clone --depth 1 https://github.com/yeyushengfan258/Tencent-icon-theme.git
 sh ~/repo/Tencent-icon-theme/install.sh
-echo "https://github.com/EliverLara/Sweet/tree/nova/kde"
-echo "https://www.pling.com/p/1305251/"
+echo "sweet: https://github.com/EliverLara/Sweet/tree/nova/kde"
+echo "candy icons: https://www.pling.com/p/1305251/"
+echo "imagination: https://www.reddit.com/r/unixporn/comments/hxqr8u/plasma_dark_plasma_theme_imagination/"
+echo "mild gradient: https://www.reddit.com/r/unixporn/comments/i14m4f/plasma_new_dark_plasma_theme_mildgradientplasma/"
+echo "cheerful phantasy: https://www.reddit.com/r/unixporn/comments/hchq3w/plasma_5_update_dark_plasma_theme_cheerfulphantasy/"
+echo "ambient blue: https://www.reddit.com/r/kde/comments/gk5sll/new_dark_plasma_theme_ambientblue/"
+echo "kvantum: ~/.local/share/themes/THEME_NAME/Kvantum/"
