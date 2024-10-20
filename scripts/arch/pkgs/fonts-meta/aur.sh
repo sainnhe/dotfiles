220c9845c4179c2f6d08b7e224af919a1b8421e3
Sainnhe Park <i@sainnhe.dev>
2024-10-20 21:45:17 +0800

    arch: add notes for fonts-meta

--- scripts/arch/pkgs/fonts-meta/aur.sh
+++ scripts/arch/pkgs/fonts-meta/aur.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+echo 'Execute "sudo usermod -aG disk $USER" to enable the user to access the disk, then logout and login again.'
+printf "Continue? [N/y] "
+read -r ans
+if [[ $ans != "y" ]]; then
+    exit 1
+fi
 pikaur -S --asdeps \
         ttf-ms-win11-auto \
         ttf-ms-win11-auto-zh_cn
