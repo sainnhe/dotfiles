3b56ebe7a15767f81422c0eccddf1bb633ae68d2
Sainnhe Park <i@sainnhe.dev>
2025-12-06 14:09:26 +0800

    scripts: update llama-server.sh

--- scripts/llama-server.sh
+++ scripts/llama-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-_linux() {
+_serve() {
     llama-server \
         --host 0.0.0.0 \
         --port 8080 \
@@ -9,18 +9,34 @@ _linux() {
         -ctk q8_0 \
         -ctv q8_0 \
         -b 2048 \
+        -ub 512 \
+        --temp 0.15 \
+        --top-k 40 \
+        --top-p 0.9 \
+        --min-p 0.05 \
+        --repeat-penalty 1.0 \
+        -fa on \
+        --cache-reuse 256 \
         -hf "$1"
     echo "==> Endpoint: http://0.0.0.0:8080/infill"
 }
 
 if [ "$(uname)" = "Linux" ]; then
     if [ "$1" = "deepseek" ]; then
-        _linux QuantFactory/DeepSeek-Coder-V2-Lite-Base-GGUF:Q4_K_S
+        _serve QuantFactory/DeepSeek-Coder-V2-Lite-Base-GGUF:Q4_K_S
     elif [ "$1" = "qwen" ]; then
-        _linux QuantFactory/Qwen2.5-Coder-14B-GGUF:Q4_K_M
+        _serve QuantFactory/Qwen2.5-Coder-14B-GGUF:Q4_K_M
     elif [ -n "$1" ]; then
-        _linux "$1"
+        _serve "$1"
     else
         echo "Usage: $0 [deepseek|qwen|hf_model]"
+    fi
+elif [ "$(uname)" = "Darwin" ]; then
+    if [ "$1" = "qwen" ]; then
+        _serve unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M
+    elif [ -n "$1" ]; then
+        _serve "$1"
+    else
+        echo "Usage: $0 [qwen|hf_model]"
     fi
 fi
