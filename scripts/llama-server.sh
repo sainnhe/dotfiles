835c91c61bc1c815286c85d1504b06f9bba5d67a
Sainnhe Park <i@sainnhe.dev>
2025-12-07 11:33:01 +0800

    llama: support ipv6 && remove seed

--- scripts/llama-server.sh
+++ scripts/llama-server.sh
@@ -4,7 +4,7 @@ UNAME=$(uname)
 
 _serve() {
     llama-server \
-        --host 0.0.0.0 \
+        --host :: \
         --port 8080 \
         -ngl 99 \
         -c 16384 \
@@ -27,23 +27,19 @@ if [ "${UNAME}" = "Linux" ]; then
         _serve QuantFactory/DeepSeek-Coder-V2-Lite-Base-GGUF:Q4_K_S
     elif [ "$1" = "qwen" ]; then
         _serve QuantFactory/Qwen2.5-Coder-14B-GGUF:Q4_K_M
-    elif [ "$1" = "seed" ]; then
-        _serve mradermacher/Seed-Coder-8B-Base-GGUF:Q4_K_M
     elif [ -n "$1" ]; then
         _serve "$@"
     else
-        echo "Usage: $0 [deepseek|qwen|seed|hf_model]"
+        echo "Usage: $0 [deepseek|qwen|hf_model]"
     fi
 elif [ "${UNAME}" = "Darwin" ]; then
     if [ "$1" = "qwen" ]; then
         _serve \
             unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M \
             -hfd unsloth/Qwen2.5-Coder-0.5B-Instruct-GGUF:Q8_0
-    elif [ "$1" = "seed" ]; then
-        _serve mradermacher/Seed-Coder-8B-Base-GGUF:Q4_K_M
     elif [ -n "$1" ]; then
         _serve "$@"
     else
-        echo "Usage: $0 [qwen|seed|hf_model]"
+        echo "Usage: $0 [qwen|hf_model]"
     fi
 fi
