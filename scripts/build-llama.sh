fb0121d5d8df9e326675a3068f33c6ad44fa7405
Sainnhe Park <i@sainnhe.dev>
2025-12-14 18:41:38 +0800

    scripts: add build-llama.sh

---
+++ scripts/build-llama.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cmake -B build \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="$HOME/.local" \
+    -DGGML_BUILD_EXAMPLES=OFF \
+    -DGGML_BUILD_TESTS=OFF \
+    -DGGML_CUDA=ON \
+    -DGGML_CUDA_FA_ALL_QUANTS=ON \
+    -DGGML_LTO=ON \
+    -DGGML_NATIVE=ON \
+    -DGGML_RPC=ON
+cmake --build build --parallel "$(nproc)"
