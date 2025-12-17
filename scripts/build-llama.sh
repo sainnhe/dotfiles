4f852df59dc34c4be8fd44638e5e040ece1b95b4
Sainnhe Park <i@sainnhe.dev>
2025-12-17 12:46:25 +0800

    llama: add macos build scripts

--- scripts/build-llama.sh
+++ scripts/build-llama.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-cmake -B build \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CUDA_ARCHITECTURES="native" \
-    -DCMAKE_INSTALL_PREFIX="$HOME/.local" \
-    -DGGML_BUILD_EXAMPLES=OFF \
-    -DGGML_BUILD_TESTS=OFF \
-    -DGGML_CUDA=ON \
-    -DGGML_CUDA_FA_ALL_QUANTS=ON \
-    -DGGML_LTO=ON \
-    -DGGML_NATIVE=ON
-cmake --build build --parallel "$(nproc)"
+UNAME=$(uname)
+
+if [ "$UNAME" = "Linux" ]; then
+    cmake -B build \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_CUDA_ARCHITECTURES="native" \
+        -DCMAKE_INSTALL_PREFIX="$HOME/.local" \
+        -DGGML_BUILD_EXAMPLES=OFF \
+        -DGGML_BUILD_TESTS=OFF \
+        -DGGML_CUDA=ON \
+        -DGGML_CUDA_FA_ALL_QUANTS=ON \
+        -DGGML_LTO=ON \
+        -DGGML_NATIVE=ON
+    cmake --build build --parallel "$(nproc)"
+elif [ "$UNAME" = "Darwin" ]; then
+    cmake -B build \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$HOME/.local" \
+        -DGGML_BUILD_EXAMPLES=OFF \
+        -DGGML_BUILD_TESTS=OFF \
+        -DGGML_LTO=ON \
+        -DGGML_NATIVE=ON \
+        -DGGML_BLAS=ON \
+        -DGGML_BLAS_VENDOR=Apple \
+        -DGGML_ACCELERATE=ON \
+        -DGGML_METAL=ON \
+        -DGGML_METAL_EMBED_LIBRARY=ON
+    cmake --build build --parallel "$(sysctl -n hw.logicalcpu)"
+fi
