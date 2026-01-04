948f03c75028e497e15544dbfa4362815f0a0ffd
Sainnhe Park <i@sainnhe.dev>
2026-01-04 19:30:06 +0800

    scripts: refactor llama-server.sh

--- scripts/llama-server.sh
+++ scripts/llama-server.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-UNAME=$(uname)
+if [ "$(uname)" = "Darwin" ]; then
+    CACHE_DIR="$HOME/Library/Caches/llama.cpp"
+else
+    CACHE_DIR="$HOME/.cache/llama.cpp"
+fi
 
 _serve() {
     llama-server \
@@ -22,38 +26,17 @@ _serve() {
         "$@"
 }
 
-if [ "${UNAME}" = "Linux" ]; then
-    if [ "$1" = "deepseek" ]; then
-        _serve -hf QuantFactory/DeepSeek-Coder-V2-Lite-Base-GGUF:Q4_K_S \
-            -hfd unsloth/Qwen2.5-Coder-0.5B-Instruct-GGUF:Q8_0 \
-            --draft 5
-    elif [ "$1" = "qwen-14b" ]; then
-        _serve -hf QuantFactory/Qwen2.5-Coder-14B-GGUF:Q4_K_M \
-            -hfd unsloth/Qwen2.5-Coder-0.5B-Instruct-GGUF:Q8_0 \
-            --draft 5
-    elif [ "$1" = "qwen-7b" ]; then
-        _serve -hf QuantFactory/Qwen2.5-Coder-7B-GGUF:Q4_K_M
-    elif [ "$1" = "seed" ]; then
-        _serve --spm-infill \
-            -m ~/.cache/llama.cpp/custom/mradermacher_Seed-Coder-8B-Base-GGUF_Seed-Coder-8B-Base.Q4_K_M.edited.gguf
-    elif [ -n "$1" ]; then
-        _serve "$@"
-    else
-        echo "Usage: $0 [deepseek|qwen-14b|qwen-7b|seed|args]"
-    fi
-elif [ "${UNAME}" = "Darwin" ]; then
-    if [ "$1" = "qwen3" ]; then
-        _serve -hf unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q4_K_M \
-            -hfd unsloth/Qwen2.5-Coder-0.5B-Instruct-GGUF:Q8_0 \
-            --draft 5
-    elif [ "$1" = "qwen2.5-7b" ]; then
-        _serve -hf QuantFactory/Qwen2.5-Coder-7B-GGUF:Q4_K_M
-    elif [ "$1" = "seed" ]; then
-        _serve --spm-infill \
-            -m ~/Library/Caches/llama.cpp/custom/mradermacher_Seed-Coder-8B-Base-GGUF_Seed-Coder-8B-Base.Q4_K_M.edited.gguf
-    elif [ -n "$1" ]; then
-        _serve "$@"
-    else
-        echo "Usage: $0 [qwen3|qwen2.5-7b|seed|args]"
-    fi
+if [ "$1" = "seed" ]; then
+    _serve --spm-infill \
+        -m "$CACHE_DIR/custom/mradermacher_Seed-Coder-8B-Base-GGUF_Seed-Coder-8B-Base.Q4_K_M.edited.gguf"
+elif [ "$1" = "qwen-7b" ]; then
+    _serve -hf QuantFactory/Qwen2.5-Coder-7B-GGUF:Q4_K_M
+elif [ "$1" = "qwen-14b" ]; then
+    _serve -hf QuantFactory/Qwen2.5-Coder-14B-GGUF:Q4_K_M
+elif [ "$1" = "deepseek" ]; then
+    _serve -hf QuantFactory/DeepSeek-Coder-V2-Lite-Base-GGUF:Q4_K_S
+elif [ -n "$1" ]; then
+    _serve "$@"
+else
+    echo "Usage: $0 {seed|qwen-7b|qwen-14b|deepseek|args...}"
 fi
