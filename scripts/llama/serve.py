6e34ba6244697f5dd8125910f3b2d65fb00ddba5
Sainnhe Park <i@sainnhe.dev>
2026-03-02 11:53:10 +0800

    llama: support qwen3.5 27B dense

--- scripts/llama/serve.py
+++ scripts/llama/serve.py
@@ -209,57 +209,36 @@ def build_serve_cmd(flags) -> list[str]:
         "--threads",
         str(threads),
         "--mlock",
+        "--parallel",
+        "4",
+        "--mlock",
+        "--jinja",
     ]
-    if flags.task == "chat":
-        comm_args.append("--jinja")
 
     # Model specific args
     model_args: list[str] = []
     if flags.model == "qwen":
-        if flags.task == "fim":
-            if flags.perf == "low":
-                model_args = [
-                    "--alias",
-                    "Qwen/Qwen2.5-Coder-7B",
-                    "--hf-repo",
-                    "mradermacher/Qwen2.5-Coder-7B-i1-GGUF:Q4_K_M",
-                ]
-            elif flags.perf == "medium":
-                model_args = [
-                    "--alias",
-                    "Qwen/Qwen3-Coder-30B-A3B-Instruct",
-                    "--hf-repo",
-                    "unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:IQ4_NL",
-                ]
-            else:
-                model_args = [
-                    "--alias",
-                    "Qwen/Qwen3-Coder-30B-A3B-Instruct",
-                    "--hf-repo",
-                    "unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF:Q8_K_XL",
-                ]
+        if flags.perf == "low":
+            model_args = [
+                "--alias",
+                "Qwen/Qwen3-8B",
+                "--hf-repo",
+                "unsloth/Qwen3-8B-GGUF:IQ4_NL",
+            ]
+        elif flags.perf == "medium":
+            model_args = [
+                "--alias",
+                "Qwen/Qwen3.5-35B-A3B",
+                "--hf-repo",
+                "unsloth/Qwen3.5-35B-A3B-GGUF:UD-Q4_K_M",
+            ]
         else:
-            if flags.perf == "low":
-                model_args = [
-                    "--alias",
-                    "Qwen/Qwen3-8B",
-                    "--hf-repo",
-                    "unsloth/Qwen3-8B-GGUF:IQ4_NL",
-                ]
-            elif flags.perf == "medium":
-                model_args = [
-                    "--alias",
-                    "Qwen/Qwen3.5-35B-A3B",
-                    "--hf-repo",
-                    "unsloth/Qwen3.5-35B-A3B-GGUF:UD-Q4_K_M",
-                ]
-            else:
-                model_args = [
-                    "--alias",
-                    "Qwen/Qwen3.5-35B-A3B",
-                    "--hf-repo",
-                    "unsloth/Qwen3.5-35B-A3B-GGUF:UD-Q8_K_XL",
-                ]
+            model_args = [
+                "--alias",
+                "Qwen/Qwen3.5-27B",
+                "--hf-repo",
+                "unsloth/Qwen3.5-27B-GGUF:UD-Q8_K_XL",
+            ]
     elif flags.model == "seed":
         if flags.task == "fim":
             # Modified version of mradermacher/Seed-Coder-8B-Base-i1-GGUF
